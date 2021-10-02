5dff326380899d3754cdf8bb91a7cbf39353a70a
jsadowski2000 <jsadowski2000@gmail.com>
2021-10-02 18:47:41 +0200

    ćw.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Jakub");
+        System.out.print("Janusz");
+        System.out.printf("Janusz %n Jakub");
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
