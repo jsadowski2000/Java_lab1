ad0ba805eb9efee3caf7a7901b1231e8a663a541
jsadowski2000 <jsadowski2000@gmail.com>
2021-10-02 19:23:34 +0200

    Merge remote-tracking branch 'origin/ćw.1' into ćw.1
    # Conflicts:
    #	out/production/java_lab_1/Main.class
    #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,19 @@ public class Main {
     public static void main(String[] args) {
 
         int zmiennaInt = 7;
+        double zmiennaDouble = 2.5;
+        char znak = 'A';
+        byte liczbaByte = 123;
+        long wielkaLiczba;
+        wielkaLiczba = 100000L;
+
+
         System.out.println("zmienna int = " + zmiennaInt);
+        System.out.println("zmienna double = " + zmiennaDouble);
+        System.out.println("Znak char = " + znak);
+        System.out.println("Liczba Byte = " + liczbaByte);
+        System.out.println("Wielka Liczba = " + wielkaLiczba);
+
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
