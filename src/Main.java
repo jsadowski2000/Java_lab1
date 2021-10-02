c2811ca5b3e41469e9f43a6c58e217292997ac77
jsadowski2000 <jsadowski2000@gmail.com>
2021-10-02 18:47:41 +0200

    ćw.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Jakub");
+        System.out.print("Janusz");
+        String imie = "Jakub";
+        int dzienUrodzin = 29;
+
+        System.out.printf("Mam na imie: %s, urodzilem sie: %d%n", imie, dzienUrodzin);
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
